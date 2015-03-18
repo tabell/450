2c2334f21ac1572ed802ed5db610ecb42809608a
Alex <tabell@uvic.ca>
2015-03-18 12:55:51 -0700

    test

--- cpu.vhd
+++ cpu.vhd
@@ -48,18 +48,26 @@ signal de_reg_write_data     : std_ulogic_vector(7 downto 0) := (others => 'Z');
 signal de_reg_write_addr     : std_ulogic_vector(1 downto 0) := (others => 'Z');
 signal de_reg_write_en       : std_ulogic := '0';
 signal de_alu_mode           : std_ulogic_vector(2 downto 0) := (others => 'Z');
+-- decode/exec control signals
+signal de_reg_data_in_sel              : std_ulogic_vector(3 downto 0) := (others => '0');
 
 -- execute/memory registers
 signal em_instr              : std_ulogic_vector(7 downto 0) := (others => 'Z');
 signal em_reg_write_addr     : std_ulogic_vector(1 downto 0) := (others => 'Z');
 signal em_reg_write_data     : std_ulogic_vector(7 downto 0) := (others => 'Z');
 signal em_reg_write_en       : std_ulogic := '0';
+-- exec/mem control signals
+signal em_reg_data_in_sel              : std_ulogic_vector(3 downto 0) := (others => '0');
 
 -- memory/writeback registers
 signal mw_instr              : std_ulogic_vector(7 downto 0) := (others => 'Z');
 
 -- control signals
-signal reg_data_in_sel              : std_ulogic_vector(3 downto 0) := (others => '0');
+signal fetch_stalled       : std_ulogic := '0';
+signal decode_stalled       : std_ulogic := '0';
+signal execute_stalled       : std_ulogic := '0';
+signal memory_stalled       : std_ulogic := '0';
+signal writeback_stalled       : std_ulogic := '0';
 
 
 begin
@@ -80,57 +88,64 @@ begin
             else
                 in_port_data <= in_port;
 -- fetch stage
-                pc <= std_ulogic_vector(unsigned(pc) + to_unsigned(1,7));
-                fd_instr <= rom_out; -- instruction register
-                fd_in_port_data <= in_port_data;
-                regfile_addr_a <= rom_out(3 downto 2);
-                regfile_addr_b <= rom_out(1 downto 0);
+                if fetch_stalled = '0' then
+                    pc <= std_ulogic_vector(unsigned(pc) + to_unsigned(1,7));
+                    fd_instr <= rom_out; -- instruction register
+                    fd_in_port_data <= in_port_data;
+                    regfile_addr_a <= rom_out(3 downto 2);
+                    regfile_addr_b <= rom_out(1 downto 0);
+                end if;
 -- decode stage
-                de_instr <= fd_instr; -- instruction register
+                if decode_stalled = '0' then
+                    de_instr <= fd_instr; -- instruction register
+                    case fd_instr(7 downto 4) is -- opcode
+                     ----- ADD ----- SUB --- NAND --- SHR ---- SHL ----------
+                    when "0100" | "0101" | "0110" | "0111" | "1000" => -- any ALU operation
+                        de_reg_write_en <= '1';
+                        de_reg_write_addr <= fd_instr(3 downto 2);
+                        -- alu mode
+                        alu_mode(2)          <= fd_instr(7);
+                        alu_mode(1 downto 0) <= fd_instr(5 downto 4);
+                        alu_in_a <= regfile_data_a;
+                        alu_in_b <= regfile_data_b;
+                        de_reg_data_in_sel <= x"0"; -- register data will  be written from alu result
+                    when "1011" => -- IN (read from input port)
+                        de_reg_write_data <= fd_in_port_data;
+                        de_reg_write_addr <= fd_instr(3 downto 2);
+                        de_reg_write_en <= '1';
+                        de_reg_data_in_sel <= x"1"; -- register data will  be written from in port
 
-                case fd_instr(7 downto 4) is -- opcode
-                 ----- ADD ----- SUB --- NAND --- SHR ---- SHL ----------
-                when "0100" | "0101" | "0110" | "0111" | "1000" => -- any ALU operation
-                    de_reg_write_en <= '1';
-                    de_reg_write_addr <= fd_instr(3 downto 2);
-                    -- alu mode
-                    alu_mode(2)          <= fd_instr(7);
-                    alu_mode(1 downto 0) <= fd_instr(5 downto 4);
-                    alu_in_a <= regfile_data_a;
-                    alu_in_b <= regfile_data_b;
-                    reg_data_in_sel <= x"0"; -- register data will  be written from alu result
-                when "1011" => -- IN (read from input port)
-                    de_reg_write_data <= fd_in_port_data;
-                    de_reg_write_addr <= fd_instr(3 downto 2);
-                    de_reg_write_en <= '1';
-                    reg_data_in_sel <= x"1"; -- register data will  be written from in port
-
-                    -- other instructions
-                    de_alu_mode <= "ZZZ";
-                when others =>
-                    de_reg_write_data <= "11111111";
-                    alu_mode <= "ZZZ";
-                    de_reg_write_en <= '0';
-                end case;
+                        -- other instructions
+                        de_alu_mode <= "ZZZ";
+                    when others =>
+                        de_reg_write_data <= "11111111";
+                        alu_mode <= "ZZZ";
+                        de_reg_write_en <= '0';
+                    end case;
+                end if;
 -- execute stage
-                em_instr <= de_instr; -- instruction register
-                em_reg_write_data <= de_reg_write_data;
-                em_reg_write_addr <= de_reg_write_addr;
-                em_reg_write_en <= de_reg_write_en;
+                if execute_stalled = '0' then
+                    em_instr <= de_instr; -- instruction register
+                    em_reg_write_data <= de_reg_write_data;
+                    em_reg_write_addr <= de_reg_write_addr;
+                    em_reg_write_en <= de_reg_write_en;
+                    em_reg_data_in_sel <= de_reg_data_in_sel;
+                end if; -- not stalled
 
 -- memory stage
-                mw_instr <= em_instr; -- instruction register
-                regfile_wr_en <= em_reg_write_en;
-                case reg_data_in_sel is
-                when x"1" =>
-                    regfile_data_w <= em_reg_write_data;
-                when x"0" =>
-                    regfile_data_w <= alu_result;
-                when others =>
-                    regfile_data_w <= alu_result;
-                end case;
-                regfile_addr_w <= em_reg_write_addr;
-
+                if memory_stalled = '0' then
+                    mw_instr <= em_instr; -- instruction register
+                    regfile_wr_en <= em_reg_write_en;
+                    case em_reg_data_in_sel is
+                    when x"1" =>
+                        regfile_data_w <= em_reg_write_data;
+                    when x"0" =>
+                        regfile_data_w <= alu_result;
+                    when others =>
+                        regfile_data_w <= alu_result;
+                    end case;
+                    regfile_addr_w <= em_reg_write_addr;
+                end if; -- not stalled
 -- writeback stage
 
             end if;
